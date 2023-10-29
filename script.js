256a764a172c7f6dfd445d783f7f9bad3e89fce8
Manas Raj <119037651+manasraj19@users.noreply.github.com>
2023-10-29 19:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,61 @@
-<!DOCTYPE html>
+*{
+	margin:0px;
+	padding:0px;
+}
+body{
+	background-color:#f5f5f5;
+}
+.container{
+	display:flex;
+	justify-content:space-evenly;
+	padding-top:10px;
  
-<html>
- 
-<head>
-	<link rel="stylesheet" href="styles.css">
-</head>
- 
-<body>
-	<!-- the entire body must be written by student -->
+}
+.header{
+	background-color:#011e29;
+	width:auto;
+	height:8vh;
+	color:white;
+	text-align:center;
+	padding-top:30px;	
+}
+.latest-news-section{
+display:flex;	
+flex-direction:column;
+	background-color:white;
+     justfy-content:space-around;
+	width:20vw;
+	border-radius:20px;
+	padding-inline:50px;
+	height:10vh;
+	text-align:center;
+}
+.pictorial-news-section{
+	display:flex;
+	flex-direction:column;
+	width:25vw;
+	background-color:white;
+	border-radius:20px;
+	height:50vh;
+	text-align:center;
+}
+.main{
+display:flex:
+flex-wrap:wrap;
+width:30vw;
+overflow:none;
+background-color:white;
+border-radius:20px;
+padding-inline:20px;
+height:30vh;
+text-align:center;
 	
-	<header class="header"><h2>Shaw's News</h2></header>
-	<div class="container">
-	<div class="latest-news-section"><h3>Latest News Article</h3>
-		<ul>
-			<li>Article 1</li>
-			<li>Article 2</li>
-			<li>Article 3</li>
-		</ul>
-	</div>
-	<div class="main">
-		<h3>Main Article</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
-	<div class="pictorial-news-section"> <h3>Pictorial News</h3>
-		<img src="https://images.hindustantimes.com/img/2023/01/29/550x309/delhi_rains_1674974112847_1674981888135_1674981888135.jpeg" alt="image1">
-		<img src="https://images.hindustantimes.com/img/2023/01/29/550x309/ADANI-INDIA-GAUTAM-ADANI-0_1675011651023_1675011651023_1675011672003_1675011672003.JPG" alt="image2">
-		<img src="https://images.hindustantimes.com/img/2023/01/29/550x309/FRANCE-REFORM-PENSIONS-31_1675022359706_1675022359706_1675022383851_1675022383851.JPG"    alt="image3">
-	</div >
-	
-    <script type="text/javascript" src="./script.js">
-</script>
-</div>
-</body>
- 
-</html>
- 
+}
+  h3 h2{
+text-align:center;
+}
+img{
+	height:15vh;
+	width:20vw;
+	padding-inline:25px;
+}
